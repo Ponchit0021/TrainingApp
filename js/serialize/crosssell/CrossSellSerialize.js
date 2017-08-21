bb30970df8cfba1a439a7a9f19d2b781f7bf32bb
Israel <israel.ramirez.ayon@gmail.com>
2017-08-21 12:51:57 -0500

    CrossSellSerialize.js - Se agrega validación para mostrar unicamente los candidatos seleccionados

--- js/serialize/crosssell/CrossSellSerialize.js
+++ js/serialize/crosssell/CrossSellSerialize.js
@@ -17,6 +17,10 @@
             oSchemaDB = new sap.ui.helper.Schema();
             this.dataDB = new sap.ui.db.Pouch(_dataDB);
             this.dataDB.setSchema(oSchemaDB.getDataDBSchema());
+            //TRAINING
+            this.crossDB = new sap.ui.db.Pouch("crossDB");
+            this.crossDB.setSchema(oSchemaDB.getCrossDBSchema());
+
             this.promoterID = "";
             this.sEntity = sEntity;
             this.sEntitySet = sEntity + "Set";
@@ -44,7 +48,7 @@
      * @return {[type]}              [description]
      */
     sap.ui.serialize.CrossSell.prototype.reviewCrossSellCandidates = function(_oType, _sPromoterID) {
-        var promiseOdata, promisePouch, oParams, oCrossSellCandidateDictionary, oDictionary,validatedcandidates, productsLength;
+        var promiseOdata, promisePouch, oParams, oCrossSellCandidateDictionary, oDictionary, validatedcandidates, productsLength;
         oDictionary = new sap.ui.helper.Dictionary();
         oParams = {
             promoterID: _sPromoterID
@@ -53,19 +57,49 @@
             oCrossSellCandidateDictionary = oDictionary.oDataRequest(oParams).getRequest(_oType);
             promisePouch = this.dataDB.get("CrossSellOfferSet");
             promiseOdata = sap.ui.getCore().AppContext.myRest.read("/" + oCrossSellCandidateDictionary.odata.name, oCrossSellCandidateDictionary.odata.get.filterCandidates, true);
+
             Promise.all([promisePouch, promiseOdata]).then(function(values) {
-                validatedcandidates={results:[]};
+                validatedcandidates = { results: [] };
 
                 _.each(values[1].results, function(item) {
-                    productsLength=_.filter(values[0].CrossSellOfferSet, function(d) { return d.CandidateIdCRM === item.CandidateIdCRM }).length;
-                    if(item.CrossSellOfferSet.results.length!==productsLength ){
+                    productsLength = _.filter(values[0].CrossSellOfferSet, function(d) { return d.CandidateIdCRM === item.CandidateIdCRM }).length;
+                    if (item.CrossSellOfferSet.results.length !== productsLength) {
                         validatedcandidates.results.push(item);
                     }
                 });
-                resolve(validatedcandidates);
-            });
+                //TRAINING - Candidatos previamente seleccionados ONLINE (Simulación)
+                this.simulateSelectedCandidates(validatedcandidates)
+                    .then(function(selectedCandidates) {
+                        resolve(selectedCandidates);
+                    });
+                //resolve(validatedcandidates);
+            }.bind(this))
         }.bind(this));
     };
+    //TRAINING - Visualización de candidatos descargados (Simulación)  
+    sap.ui.serialize.CrossSell.prototype.simulateSelectedCandidates = function(_validatedcandidates) {
+        var oDictionary = new sap.ui.helper.Dictionary();
+        var finalCandidates = { results: null };
+        var oArray = _validatedcandidates.results;
+        var oCandidates = [];
+        return new Promise(function(resolve, reject) {
+            this.crossDB.get(oDictionary.oQueues.CrossSellBatch)
+                .then(function(result) {
+                    if (result.CrossSellBatchSet) {
+                        _.each(result.CrossSellBatchSet[0].selectedCandidates, function(item) {
+                            _.each(oArray, function(candidate, i) {
+                                if (item === candidate.CandidateIdCRM) {
+                                    oCandidates.push(candidate);
+                                }
+                            });
+                        });
+                    }
+                    finalCandidates = { results: oCandidates };
+                    resolve(finalCandidates);
+                }.bind(this));
+        }.bind(this));
+    };
+
     sap.ui.serialize.CrossSell.prototype.reviewCrossSellProducts = function(_aProducts) {
         var currentProducts, currentCandidateIdCRM, pouchCrossSellOfferFiltered;
         currentProducts = _aProducts.results[0].CrossSellOfferSet.results;
